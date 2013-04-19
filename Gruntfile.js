1daec255ca21d4a246ef6c615b01512a1fc5dea7
Thanasis Polychronakis <thanpolas@gmail.com>
2013-04-19 18:21:13 +0300

    Added grunt-release

--- Gruntfile.js
+++ Gruntfile.js
@@ -87,7 +87,21 @@ module.exports = function(grunt) {
       }
 
     },
-
+    release: {
+      options: {
+        bump: true, //default: true
+        file: 'package.json', //default: package.json
+        add: true, //default: true
+        commit: true, //default: true
+        tag: true, //default: true
+        push: true, //default: true
+        pushTags: true, //default: true
+        npm: true, //default: true
+        tagName: 'v<%= version %>', //default: '<%= version %>'
+        commitMessage: 'releasing v<%= version %>', //default: 'release <%= version %>'
+        tagMessage: 'v<%= version %>' //default: 'Version <%= version %>'
+      }
+    },
     watch: {
       test: {
         files: [
